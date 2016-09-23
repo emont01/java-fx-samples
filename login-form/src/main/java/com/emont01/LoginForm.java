dc96f15ff12466acff08934b2e6db4eacf61162f
Eivar Montenegro <e.mont01@gmail.com>
2016-09-22 19:39:50 -0500

    hello world completed, login form created

---
+++ login-form/src/main/java/com/emont01/LoginForm.java
@@ -0,0 +1,22 @@
+package com.emont01;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class LoginForm extends Application
+{
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+        primaryStage.show();
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
