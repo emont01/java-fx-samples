f4a5ea21d78704fb6fe9d9fbc0a62b40eb19cb8b
Eivar Montenegro <e.mont01@gmail.com>
2016-09-30 11:25:28 -0500

    created animation example and charts example

--- charts/src/main/java/com/emont01/MainApp.java
+++ charts/src/main/java/com/emont01/MainApp.java
@@ -26,7 +26,7 @@ public class MainApp extends Application {
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 370);
+        Scene scene = new Scene(rootNode, 400, 200);
         scene.getStylesheets().add("/styles/styles.css");
 
         stage.setTitle("Hello JavaFX and Maven");
